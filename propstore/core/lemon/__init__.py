d6d6dabcbb3cef294c6af02dea0575e446b48ffa
Christopher Toth <q.alpha@gmail.com>
2026-04-16 22:23:51 -0600

    feat: add lemon lexical core

---
+++ propstore/core/lemon/__init__.py
@@ -0,0 +1,17 @@
+"""OntoLex-Lemon core objects for concept lexicalization."""
+
+from propstore.core.lemon.types import (
+    LexicalEntry,
+    LexicalForm,
+    LexicalSense,
+    OntologyReference,
+    lexical_entry_identity_key,
+)
+
+__all__ = [
+    "LexicalEntry",
+    "LexicalForm",
+    "LexicalSense",
+    "OntologyReference",
+    "lexical_entry_identity_key",
+]
