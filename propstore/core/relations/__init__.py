c952d9fb53cd313bc3f011401a9c05daa192494c
Christopher Toth <q.alpha@gmail.com>
2026-04-25 17:57:35 -0600

    Add bootstrap relation kernel

---
+++ propstore/core/relations/__init__.py
@@ -0,0 +1,19 @@
+"""Bootstrap relation kernel."""
+
+from __future__ import annotations
+
+from propstore.core.relations.kernel import (
+    BOOTSTRAP_RELATION_IDS,
+    RelationConceptRef,
+    RoleBinding,
+    RoleBindingSet,
+    RoleSignature,
+)
+
+__all__ = [
+    "BOOTSTRAP_RELATION_IDS",
+    "RelationConceptRef",
+    "RoleBinding",
+    "RoleBindingSet",
+    "RoleSignature",
+]
