c1ce8505681333b5d4c173ef6bdd1f67fcfc6447
Christopher Toth <q.alpha@gmail.com>
2026-04-25 18:14:39 -0600

    Add situated assertion reference types

---
+++ propstore/core/assertions/__init__.py
@@ -0,0 +1,15 @@
+"""Core situated assertion reference types."""
+
+from __future__ import annotations
+
+from propstore.core.assertions.refs import (
+    UNCONDITIONAL_CONDITION_REF,
+    ConditionRef,
+    ProvenanceGraphRef,
+)
+
+__all__ = [
+    "UNCONDITIONAL_CONDITION_REF",
+    "ConditionRef",
+    "ProvenanceGraphRef",
+]
