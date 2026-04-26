12a0ec95bfb0fc896693246a9115a9ecbdb249a2
Christopher Toth <q.alpha@gmail.com>
2026-04-25 21:23:31 -0600

    Use public gunray complement boundary

--- propstore/grounding/gunray_complement.py
+++ propstore/grounding/gunray_complement.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from gunray.disagreement import complement as gunray_complement
-from gunray.types import GroundAtom
+from gunray import GroundAtom, complement
 
 
 class GunrayComplementEncoder:
     def complement(self, predicate: str) -> str:
-        return gunray_complement(GroundAtom(predicate=predicate, arguments=())).predicate
+        return complement(GroundAtom(predicate=predicate, arguments=())).predicate
 
 
 GUNRAY_COMPLEMENT_ENCODER = GunrayComplementEncoder()
