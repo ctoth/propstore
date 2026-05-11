7266da7b3c196ac2a5d54b8da2ecef01afa1200c
Christopher Toth <q.alpha@gmail.com>
2026-05-11 00:08:13 -0600

    Remove unused JSON TypeVar

--- propstore/json_types.py
+++ propstore/json_types.py
@@ -2,9 +2,8 @@
 
 from __future__ import annotations
 
-from typing import TypeAlias, TypeVar
+from typing import TypeAlias
 
 JsonScalar: TypeAlias = None | bool | int | float | str
 JsonValue: TypeAlias = JsonScalar | list["JsonValue"] | dict[str, "JsonValue"]
 JsonObject: TypeAlias = dict[str, JsonValue]
-JsonValueT = TypeVar("JsonValueT", bound=JsonValue)
