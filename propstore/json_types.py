041e779e9cec512b8275b42e8309bde506860b87
Christopher Toth <q.alpha@gmail.com>
2026-05-11 00:05:54 -0600

    Add generic JSON value type variable

--- propstore/json_types.py
+++ propstore/json_types.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
-from typing import TypeAlias
+from typing import TypeAlias, TypeVar
 
 JsonScalar: TypeAlias = None | bool | int | float | str
 JsonValue: TypeAlias = JsonScalar | list["JsonValue"] | dict[str, "JsonValue"]
 JsonObject: TypeAlias = dict[str, JsonValue]
+JsonValueT = TypeVar("JsonValueT", bound=JsonValue)
