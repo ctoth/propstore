1faf7f6e578f461dbca1205efbbf69f1a8e9edcd
Christopher Toth <q.alpha@gmail.com>
2026-05-21 17:35:00 -0600

    Delete semantic id conversion helpers

--- propstore/core/id_types.py
+++ propstore/core/id_types.py
@@ -1,8 +1,7 @@
-"""Semantic ID newtypes and coercion helpers."""
+"""Semantic ID newtypes."""
 
 from __future__ import annotations
 
-from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import NewType
 
@@ -31,55 +30,3 @@ class LogicalId:
             "namespace": self.namespace,
             "value": self.value,
         }
-
-
-def to_concept_id(value: object) -> ConceptId:
-    return ConceptId(str(value))
-
-
-def to_claim_id(value: object) -> ClaimId:
-    return ClaimId(str(value))
-
-
-def to_assertion_id(value: object) -> AssertionId:
-    return AssertionId(str(value))
-
-
-def to_context_id(value: object) -> ContextId:
-    return ContextId(str(value))
-
-
-def to_condition_id(value: object) -> ConditionId:
-    return ConditionId(str(value))
-
-
-def to_provenance_graph_id(value: object) -> ProvenanceGraphId:
-    return ProvenanceGraphId(str(value))
-
-
-def to_justification_id(value: object) -> JustificationId:
-    return JustificationId(str(value))
-
-
-def to_assumption_id(value: object) -> AssumptionId:
-    return AssumptionId(str(value))
-
-
-def to_queryable_id(value: object) -> QueryableId:
-    return QueryableId(str(value))
-
-
-def to_concept_ids(values: Iterable[object]) -> tuple[ConceptId, ...]:
-    return tuple(to_concept_id(value) for value in values)
-
-
-def to_claim_ids(values: Iterable[object]) -> tuple[ClaimId, ...]:
-    return tuple(to_claim_id(value) for value in values)
-
-
-def to_assumption_ids(values: Iterable[object]) -> tuple[AssumptionId, ...]:
-    return tuple(to_assumption_id(value) for value in values)
-
-
-def to_queryable_ids(values: Iterable[object]) -> tuple[QueryableId, ...]:
-    return tuple(to_queryable_id(value) for value in values)
